aaa0719f26c4638f87a0e6f6e18b416e3078619f
Alessandro Candolini <alessandrocandolini@gmail.com>
2022-11-23 03:08:25 +0000

    implement parse function

--- src/Todo.hs
+++ src/Todo.hs
@@ -1,58 +1,65 @@
-{-# LANGUAGE GADTs #-}
-module Todo where
-import Data.Time
-import Data.Either.Combinators (maybeToRight)
-import Data.Map
+module Todo (program, program', Todo, Input, parse) where
+import Data.Time ( getCurrentTime, UTCTime )
+import Data.Map ()
+import Data.List.NonEmpty ( NonEmpty, nonEmpty )
+import Data.Either.Combinators ( maybeToRight )
+import Control.Monad (void,mfilter)
+import Data.Time.ISO8601 ( parseISO8601 )
+
+type NonEmptyString = NonEmpty Char
 
 newtype DueDate = DueDate UTCTime deriving (Eq,Show,Ord)
 
-newtype Description = Description String deriving (Eq,Show)
+newtype Description = Description NonEmptyString deriving (Eq,Show)
 
 data Input = Input String (Maybe String) deriving (Eq,Show)
 data Todo = Todo Description (Maybe DueDate) deriving (Eq,Show)
 
+persist :: Todo -> IO ()
+persist = void . pure
 
-persist :: Todo -> IO () 
-persist = undefined
+data Error = DescriptionEmpty | PastDueDate | InvalidDateFormat deriving (Eq,Show)
 
-data Error = DescriptionEmpty | PastDueDate | InvalidDateFormat deriving (Eq,Show) 
-
-serialise :: Error -> String 
-serialise = show 
+serialise :: Error -> String
+serialise = show
 
 render :: Error -> IO ()
-render = putStrLn . serialise 
+render = putStrLn . serialise
 
-parse :: UTCTime -> Input -> Either Error Todo 
---parse (Input [] _ ) = Left DescriptionEmpty
---parse (Input d Nothing) = Right (Todo (Description d) Nothing) 
---parse (Input d (Just s)) =  undefined 
-parse now = undefined 
+parse :: UTCTime -> Input -> Either Error Todo
+parse now (Input s d) =
+    Todo <$>
+     maybeToRight DescriptionEmpty (processDescription s) <*>
+     processDate now d
 
-parseDate :: String -> Maybe UTCTime
-parseDate = undefined
+processDescription :: String -> Maybe Description
+processDescription = fmap Description . nonEmpty
 
-data Action = Persist Todo | Render Error  | GetCurrentTime (UTCTime -> Action)  
+processDate :: UTCTime -> Maybe String -> Either Error (Maybe DueDate)
+processDate now =
+  traverse (fmap DueDate . (=<<) (maybeToRight PastDueDate . mfilter (> now) . Just) . maybeToRight InvalidDateFormat  . parseISO8601)
 
-run :: Action -> IO () 
-run (Persist t) = persist t 
-run (Render e) = render e 
-run (GetCurrentTime f) = getCurrentTime >>= run . f 
+data Action = Persist Todo | Render Error  | GetCurrentTime (UTCTime -> Action)
+
+run :: Action -> IO ()
+run (Persist t) = persist t
+run (Render e) = render e
+run (GetCurrentTime f) = getCurrentTime >>= run . f
 
 program' :: Input -> Action
-program' input = 
-   GetCurrentTime (\ now -> 
-    case parse now input of 
-     Right todo -> Persist todo  
+program' input =
+   GetCurrentTime (\ now ->
+    case parse now input of
+     Right todo -> Persist todo
      Left e -> Render e )
 
 program :: Input -> IO ()
-program = run . program' 
+program = run . program'
 
-original :: Input -> IO ()
-original input = 
-   getCurrentTime >>= (\ now -> 
-    case parse now input of 
-     Right todo -> persist todo  
-     Left e -> render e )
+--original :: Input -> IO ()
+--original input =
+   --getCurrentTime >>= (\ now ->
+    --case parse now input of
+     --Right todo -> persist todo
+     --Left e -> render e )
 
